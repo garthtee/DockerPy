9a3468d7f7fddae9de5078e4b27fc3eb70fb7fae
Garth Toland <tolandgarth@gmail.com>
2017-03-22 23:04:56 +0000

    Added pip upgrade to Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -7,6 +7,7 @@ FROM ubuntu:latest
 MAINTAINER Garth Toland "tolandgarth@gmail.com"
 RUN apt-get update -y
 RUN apt-get install -y python-pip python-dev build-essential
+RUN pip install --upgrade pip
 COPY . /app
 WORKDIR /app
 RUN pip install -r requirements.txt
--- web/app.py
+++ web/app.py
@@ -2,9 +2,10 @@ from flask import Flask
 from misc.print_method import *
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_world():
     return 'Flask Dockerized :) ' + str(print_hello())
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0')
